36ab2a1a8ebe771976608a039a37797a849ce765
NishNovae <raecrowned@gmail.com>
2024-07-24 19:11:35 +0900

    fixing merge

--- src/my_history/cli.py
+++ src/my_history/cli.py
@@ -1,24 +1,5 @@
 import sys
 import pandas as pd
-<<<<<<< HEAD
-
-def cnt():
-    args = sys.argv
-    try:
-        if len(args) != 2:
-            print("ERROR: requires one in-line argument. Please try again.")
-        else:
-            try:
-                df = pd.read_parquet("~/tmp/history.parquet")
-                fdf = df[df['cmd'].str.contains(args[1])]
-                cnt = fdf['cnt'].sum()
-                print(cnt)
-            except ValueError:
-                print("Something went wrong...")
-    except AttributeError:
-        print("ERROR: sys.argv does not exist. The script should be run from the command line with in-line arguments.")
-        
-=======
 import argparse
 
 PARQ_PATH="/home/nishtala/db/parquet/"
@@ -59,4 +40,3 @@ def cnt_top(args):          # list of 2 elements [0], [1]
     print("================================================")
     print(fdf.head(num))
 #argv()
->>>>>>> fe3b1b0 (vers)
